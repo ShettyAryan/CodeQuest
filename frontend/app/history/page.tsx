db683652044614151485e241e46c228bfb304033
Aryan Shetty <shetty.aryan1923@gmail.com>
2025-07-19 19:30:44 +0530

    Frontend complete with integration

--- frontend/app/history/page.tsx
+++ frontend/app/history/page.tsx
@@ -1,19 +1,31 @@
 "use client";
 import MCQChallenge from "@/sections/MCQChallenge";
-import { Divide } from "lucide-react";
+import { useApi } from "@/util/api";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
+
 import React, { useEffect, useState } from "react";
 
 const page = () => {
+  const { makeRequest } = useApi();
   const [history, setHistory] = useState<[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     fetchHistory();
   }, []);
 
   const fetchHistory = async () => {
-    setIsLoading(false);
+    setIsLoading(true);
+    setError(null);
+    try {
+      const data = await makeRequest("my-history");
+      setHistory(data.challenges);
+    } catch (err) {
+      setError("Failed to load history");
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   if (isLoading) {
@@ -28,28 +40,35 @@ const page = () => {
     );
   }
   return (
-    <div className="bg-black h-screen w-full flex items-center flex-col gap-4 pt-30">
-      <div className="bg-[#1E293B] min-w-[90vw] py-10 px-10  rounded-xl">
-        <h2 className="text-5xl text-white text-center">History</h2>
-        {history.length === 0 ? (
-          <p className="text-lg font-medium text-gray-300">
-            No Challenge History
-          </p>
-        ) : (
-          <div>
-            {history.map((challenge) => {
-              return (
-                <MCQChallenge
-                  challenge={challenge}
-                  key={challenge.id}
-                  showExplaination
-                />
-              );
-            })}
+    <>
+      <SignedIn>
+        <div className="bg-black min-h-screen w-full flex items-center flex-col gap-4 pt-30">
+          <div className="bg-[#1E293B] min-w-[90vw] py-10 px-10  rounded-xl">
+            <h2 className="text-5xl text-white text-center">History</h2>
+            {history.length === 0 ? (
+              <p className="text-lg font-medium text-gray-300">
+                No Challenge History
+              </p>
+            ) : (
+              <div className="px-20 py-10 flex flex-col gap-4">
+                {history.map((challenge) => {
+                  return (
+                    <MCQChallenge
+                      challenge={challenge}
+                      key={challenge.id}
+                      showExplaination
+                    />
+                  );
+                })}
+              </div>
+            )}
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
   );
 };
 
--- frontend/sections/MCQChallenge.tsx
+++ frontend/sections/MCQChallenge.tsx
@@ -1,23 +1,37 @@
 "use client";
 import React from "react";
+import { useState, useEffect } from "react";
 
 const MCQChallenge = ({
   challenge,
   showExplaination = false,
 }: {
-  challenge: string;
+  challenge: {
+    id: number;
+    title: string;
+    options: string[] | string;
+    correct_answer_id: number;
+    explanation: string;
+    difficulty: string;
+    last_reset_date?: string;
+  };
   showExplaination: boolean;
 }) => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null);
   const [shouldShowExplaination, setShouldShowExplaination] =
     useState<boolean>(showExplaination);
 
+  useEffect(() => {
+    setSelectedOption(null);
+    setShouldShowExplaination(false);
+  }, [challenge]);
+
   const options =
     typeof challenge.options === "string"
       ? JSON.parse(challenge.options)
       : challenge.options;
 
-  const handleOptionSelect = (index) => {
+  const handleOptionSelect = (index: string | null) => {
     if (selectedOption !== null) return;
     setSelectedOption(index);
     setShouldShowExplaination(true);
@@ -35,13 +49,19 @@ const MCQChallenge = ({
   };
   return (
     <div className="bg-[#1E293B] flex flex-col items-center">
-      <p className="text-lg font-semibold text-white">{challenge.difficulty}</p>
-      <div className="min-h-[40px]">{challenge.title}</div>
+      <p className="text-lg font-medium text-white py-2 capitalize">
+        Difficulty : {challenge.difficulty}
+      </p>
+      <div className="min-h-[40px] w-full px-10 text-white font-bold text-xl">
+        {challenge.title}
+      </div>
 
       <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-2 px-10 py-4">
-        {options.map((option, index) => (
+        {options.map((option, index: string | null) => (
           <div
-            className="w-2xl px-2 py-5 bg-[#334155]"
+            className={`w-2xl px-2 py-5 text-white text-lg cursor-pointer rounded transition-colors duration-300 ${getOptionClass(
+              index
+            )}`}
             key={index}
             onClick={() => {
               handleOptionSelect(index);
@@ -52,7 +72,7 @@ const MCQChallenge = ({
         ))}
       </div>
       {shouldShowExplaination && selectedOption !== null && (
-        <div className="bg-[#0F172A] w-full px-10 py-2 flex flex-col gap-3">
+        <div className="bg-[#0F172A] w-full px-10 py-10 flex flex-col gap-3">
           <h4 className="text-xl font-semibold text-white">Explaination:</h4>
           <p className="text-lg font-medium text-gray-400">
             {challenge.explanation}
